563e275e74886601f9a26c3903fb79921876be0f
askirov <kirov_as@i.ua>
2019-07-17 17:58:57 +0300

    scroll to id

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+if ($(window).width() < 768) {
+	$('.nav-tabs a').on('shown.bs.tab', function () {
+    $('html, body').animate({
+        scrollTop: $('.tab-content').offset().top
+    },
+    500,
+    'linear'
+  )
+});
+}
+});
