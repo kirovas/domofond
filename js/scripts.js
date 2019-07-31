ec3b5fb60d239f663697f00d4851b3f75a1c3a79
askirov <kirov_as@i.ua>
2019-07-31 23:08:04 +0300

    new 11 pages

--- js/scripts.js
+++ js/scripts.js
@@ -9,4 +9,38 @@ if ($(window).width() < 768) {
   )
 });
 }
+
+
+jQuery('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="images/num-up.png" alt=""></div><div class="quantity-button quantity-down"><img src="images/num-down.png" alt=""></div></div>').insertAfter('.quantity input');
+    jQuery('.quantity').each(function() {
+      var spinner = jQuery(this),
+        input = spinner.find('input[type="number"]'),
+        btnUp = spinner.find('.quantity-up'),
+        btnDown = spinner.find('.quantity-down'),
+        min = input.attr('min'),
+        max = input.attr('max');
+
+      btnUp.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue + 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+      btnDown.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue - 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+    });
 });
